03ba54d76c0f9e4f24c27e796e4e4f8bb8fc3779
Hud-Tariq <houdtariq@gmail.com>
2025-12-14 23:31:54 -0800

    Refactor tree and list classes for consistency

--- Trees.cpp
+++ Trees.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <queue>
+#include <algorithm> 
 using namespace std;
 
 template <typename T>
-class Node {
+class Node
+{
 public:
     T data;
     Node<T>* next_node;
     Node<T>* prev_node;
 
     Node(T val = T(), Node<T>* prev = nullptr, Node<T>* next = nullptr)
-        : data(val), prev_node(prev), next_node(next) {}
+        : data(val), prev_node(prev), next_node(next) {
+    }
 };
 
 template <typename T>
-class List {
+class List
+{
 private:
     Node<T>* head_node;
     Node<T>* tail_node;
@@ -22,8 +26,10 @@ private:
 public:
     List() : head_node(nullptr), tail_node(nullptr) {}
 
-    ~List() {
-        while (head_node != nullptr) {
+    ~List()
+    {
+        while (head_node != nullptr)
+        {
             Node<T>* temp = head_node;
             head_node = head_node->next_node;
             delete temp;
@@ -34,20 +40,24 @@ public:
 
     Node<T>* head() const { return head_node; }
 
-    void push_back(T val) {
+    void push_back(T val)
+    {
         Node<T>* n = new Node<T>(val, tail_node, nullptr);
-        if (empty()) {
+        if (empty())
+        {
             head_node = tail_node = n;
-        } else {
+        }
+        else
+        {
             tail_node->next_node = n;
             tail_node = n;
         }
     }
 };
 
-
 template <typename T>
-class Simple_tree {
+class Simple_tree
+{
 private:
     T obj;
     Simple_tree<T>* parent_node;
@@ -56,93 +66,133 @@ public:
     List<Simple_tree<T>*> children;
 
     Simple_tree(T e = T(), Simple_tree<T>* p = nullptr)
-        : obj(e), parent_node(p) {}
+        : obj(e), parent_node(p) {
+    }
 
     T& value() { return obj; }
 
     Simple_tree<T>* parent() const { return parent_node; }
 
+    bool is_root() const { return parent_node == nullptr; }
+
     bool is_leaf() const { return children.empty(); }
 
-    void add_child(Simple_tree<T>* child) {
+    void add_child(Simple_tree<T>* child)
+    {
         child->parent_node = this;
         children.push_back(child);
     }
 
-    int depth() const {
-        if (parent_node == nullptr)
+    int degree() const
+    {
+        int count = 0;
+        for (Node<Simple_tree<T>*>* ptr = children.head();
+            ptr != nullptr; ptr = ptr->next_node)
+            count++;
+        return count;
+    }
+
+    int size() const
+    {
+        int count = 1;
+        for (Node<Simple_tree<T>*>* ptr = children.head();
+            ptr != nullptr; ptr = ptr->next_node)
+            count += ptr->data->size();
+        return count;
+    }
+
+    int height() const
+    {
+        if (is_leaf())
             return 0;
-        return parent_node->depth() + 1;
+
+        int max_h = 0;
+        for (Node<Simple_tree<T>*>* ptr = children.head();
+            ptr != nullptr; ptr = ptr->next_node)
+        {
+            max_h = max(max_h, ptr->data->height());
+        }
+        return max_h + 1;
     }
 };
 
-
 template <typename T>
-void preorder(Simple_tree<T>* root) {
-    if (!root) return;
+void preorder(Simple_tree<T>* root)
+{
+    if (!root)
+        return;
 
     cout << root->value() << " ";
 
     for (Node<Simple_tree<T>*>* ptr = root->children.head();
-         ptr != nullptr; ptr = ptr->next_node)
+        ptr != nullptr; ptr = ptr->next_node)
         preorder(ptr->data);
 }
 
 template <typename T>
-void postorder(Simple_tree<T>* root) {
-    if (!root) return;
+void postorder(Simple_tree<T>* root)
+{
+    if (!root)
+        return;
 
     for (Node<Simple_tree<T>*>* ptr = root->children.head();
-         ptr != nullptr; ptr = ptr->next_node)
+        ptr != nullptr; ptr = ptr->next_node)
         postorder(ptr->data);
 
     cout << root->value() << " ";
 }
 
 template <typename T>
-void levelorder(Simple_tree<T>* root) {
-    if (!root) return;
+void levelorder(Simple_tree<T>* root)
+{
+    if (!root)
+        return;
 
     queue<Simple_tree<T>*> q;
     q.push(root);
 
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         Simple_tree<T>* cur = q.front();
         q.pop();
 
         cout << cur->value() << " ";
 
         for (Node<Simple_tree<T>*>* ptr = cur->children.head();
-             ptr != nullptr; ptr = ptr->next_node)
+            ptr != nullptr; ptr = ptr->next_node)
             q.push(ptr->data);
     }
 }
 
-int main() {
-    Simple_tree<int>* root = new Simple_tree<int>(1);
-
-    auto c1 = new Simple_tree<int>(2);
-    auto c2 = new Simple_tree<int>(3);
-    auto c3 = new Simple_tree<int>(4);
-
-    root->add_child(c1);
-    root->add_child(c2);
-    root->add_child(c3);
-
-    c1->add_child(new Simple_tree<int>(5));
-    c1->add_child(new Simple_tree<int>(6));
-
-    cout << "Preorder: ";
-    preorder(root);
+int main()
+{
+    Simple_tree<char>* a = new Simple_tree<char>('H');
+    Simple_tree<char>* b = new Simple_tree<char>('U');
+    Simple_tree<char>* c = new Simple_tree<char>('D');
+    Simple_tree<char>* d = new Simple_tree<char>('T');
+    Simple_tree<char>* e = new Simple_tree<char>('A');
+    Simple_tree<char>* f = new Simple_tree<char>('R');
+    Simple_tree<char>* g = new Simple_tree<char>('I');
+    Simple_tree<char>* h = new Simple_tree<char>('Q');
+    a->add_child(b);
+    a->add_child(c);
+    b->add_child(d);
+    b->add_child(e);
+    c->add_child(f);
+    c->add_child(g);
+    d->add_child(h);
+    cout << "DFS (Preorder): ";
+    preorder(a);
     cout << endl;
 
-    cout << "Postorder: ";
-    postorder(root);
+    cout << "BFS (Level Order): ";
+    levelorder(a);
     cout << endl;
 
-    cout << "Level-order: ";
-    levelorder(root);
-    cout << endl;
+    cout << "Height of tree" << endl;
+    cout << a->height() << endl;
 
+    cout << "Degree of tree" << endl;
+    cout << a->degree() << endl;
     return 0;
 }
